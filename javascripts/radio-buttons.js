1a27e34ebc8272ad04ea628c42143d82ac21515d
Ben Hodge <benjamin.hodge@leeds.gov.uk>
2019-10-11 09:37:15 +0100

    JS trigger for radio buttons

--- javascripts/radio-buttons.js
+++ javascripts/radio-buttons.js
@@ -1,19 +1,29 @@
 (function (global, $) {
-    if($('.form-group--radio').length > 0) {
-        $('.form-group--radio > input').on('click', function() {
-            $('.form-group--radio').removeClass('display');
-            $(this).parent().addClass('display');
-        });
-    
-        $('.option--sub > input').on('click', function() {
-            $('.option--sub').removeClass('display');
-            $(this).parent().addClass('display');
-        });
-    
-    } else {
-        $('input:radio').click(function() {
-            $('input:radio[name='+$(this).attr('name')+']').parent().removeClass('active');
-                $(this).parent().addClass('active');
-        });
+
+    var LCC = global.LCC || {};
+	LCC.radioButtons = LCC.radioButtons || {};
+
+    LCC.radioButtons.conditional = function () {
+
+        if($('.form-group--radio').length > 0) {
+            $('.form-group--radio > input').on('click', function() {
+                $('.form-group--radio').removeClass('display');
+                $(this).parent().addClass('display');
+            });
+        
+            $('.option--sub > input').on('click', function() {
+                $('.option--sub').removeClass('display');
+                $(this).parent().addClass('display');
+            });
+        
+        } else {
+            $('input:radio').click(function() {
+                $('input:radio[name='+$(this).attr('name')+']').parent().removeClass('active');
+                    $(this).parent().addClass('active');
+            });
+        }
+
     }
-});
+
+    global.LCC = LCC;
+})(window, jQuery);
