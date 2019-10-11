e78b476da38bb9377cac9c5609549d287a7bcc58
Ben Hodge <benjamin.hodge@leeds.gov.uk>
2019-10-11 14:21:38 +0100

    adding select conditional

--- javascripts/radio-buttons.js
+++ javascripts/radio-buttons.js
@@ -1,10 +1,9 @@
 (function (global, $) {
 
     var LCC = global.LCC || {};
-	LCC.radioButtons = LCC.radioButtons || {};
-
-    LCC.radioButtons.conditional = function () {
-
+    LCC.formConditionals = LCC.formConditionals || {};
+    
+    LCC.formConditionals.radio = function() {
         if($('.form-group--radio').length > 0) {
             $('.form-group--radio > input').on('click', function() {
                 $('.form-group--radio').removeClass('display');
@@ -22,7 +21,19 @@
                     $(this).parent().addClass('active');
             });
         }
+    }
 
+    LCC.formConditionals.select = function() {
+        // Select - other
+        if( $('.form-control').length > 0 ) {
+            $('select').on('change', function() {
+                $(this).parent().removeClass('active');
+                var value = this.value;
+                if( value == 'Other' ) {
+                    $(this).parent().addClass('active');
+                }
+            });
+        }
     }
 
     global.LCC = LCC;
